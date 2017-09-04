9102f441e168f5252717304ab6d5afbf55f5a9e9
cernst11 <cernst11@gmail.com>
2017-09-04 00:10:36 -0500

    Add images for manifest

--- js/index.js
+++ js/index.js
@@ -1,27 +1,32 @@
-import fileSaver from 'file-Saver';
+//Internal classes
 import CanvasClass from './CanvasClass';
 import LocationLookup from './LocationLookup';
 import Map from './Map.js';
-import TextOverlay from './TextOverlay';
+import PosterStyling from './PosterStyling';
 import keys from './keys';
 import Styles from './Styles';
+
+//External libs
+import fileSaver from 'file-Saver';
+import html2canvas  from 'html2canvas';
+import mapboxgl from 'mapbox-gl';
+require('offline-plugin/runtime').install();
+
+//App manifest for pwa
 require('./manifest.json');
+//css
+import '../css/font.css';
+import '../css/input.css';
+import '../css/main.css';
+import '../css/main-overlay.css';
+
+//images
 require('../images/manifest/icon-512x512.png');
 require('../images/manifest/icon-192x192.png');
 require('../images/manifest/icon-144x144.png');
 require('../images/manifest/icon-96x96.png');
 require('../images/manifest/icon-72x72.png');
 require('../images/manifest/icon-48x48.png');
-
-
-import html2canvas  from 'html2canvas';
-import mapboxgl from 'mapbox-gl';
-require('offline-plugin/runtime').install();
-
-import '../css/font.css';
-import '../css/input.css';
-import '../css/main.css';
-import '../css/main-overlay.css';
 
 function updateDPI(dpi = 300) {
     Object.defineProperty(window, 'devicePixelRatio', {
@@ -35,19 +40,17 @@ updateDPI();
 
 let map = new Map(keys().mapBox)
 
-
 let  locationLookupCallback = ()=> {
-    let textOverlay  = new TextOverlay;
-    let locationLookup = new LocationLookup(textOverlay, map );
+    let posterStyling  = new PosterStyling(map);
+    let locationLookup = new LocationLookup(posterStyling, map );
     locationLookup.locationLookupLoaded();
     let canvasClass = new CanvasClass(1800, 2400);
 }
 
-window.Buffer = null;
-
+//set this to windows scope so the callback picks it up
 window.locationLookupCallback = locationLookupCallback;
 
-
+//set up some things for when the documnet loads
 document.addEventListener("DOMContentLoaded", function(event) { 
     //set up the google maps api. We need to pass in the key from the keys.js file
     let script = document.createElement('script');
