152fb624e614b1c629e50421384030a1e7a0ff4b
cernst11 <cernst11@gmail.com>
2017-09-30 16:11:07 -0500

    Auto update country and city name

--- js/locationLookup.js
+++ js/locationLookup.js
@@ -24,11 +24,36 @@ export default class LocationLookup {
         let autoComplete = new google.maps.places.Autocomplete(addressField, options);
         let that = this;
         autoComplete.addListener('place_changed', () => {
-            that.textOverlay.location = this.textOverlay.formatCoord(autoComplete.getPlace().geometry.location.lat(),
+            this.textOverlay.country = this.getCountryName(autoComplete.getPlace());
+            this.textOverlay.city = this.getCityName(autoComplete.getPlace());
+            this.textOverlay.location = this.textOverlay.formatCoord(autoComplete.getPlace().geometry.location.lat(),
                 autoComplete.getPlace().geometry.location.lng())
             that.mapBox.map.setCenter([autoComplete.getPlace().geometry.location.lng(),
                 autoComplete.getPlace().geometry.location.lat()
             ]);
         });
+    }
+
+    /**
+     * Filter for country based on type. If unites staes convert to usa
+     * @param {Object} places The places object to filter
+     */
+    //TODO: create function to map long country name to short country name for long country names ie usa uae etc..
+    getCountryName(places) {
+        let country = places.address_components.filter((component) => {
+            return (component.types.includes("country"))
+        })
+        return country[0].long_name.toUpperCase() === 'UNITED STATES' ? 'USA' : country[0].long_name;
+    }
+
+    /**
+     * Filter for city based on type.
+     * @param {Object} places The places object to filter
+     */
+    getCityName(places) {
+        let city = places.address_components.filter((component) => {
+            return (component.types.includes("locality") && component.types.includes("political"));
+        })
+        return city[0].long_name;
     }
 }
