48c4b3f35ea8efc8e7e0669bbcb604ddc9adf839
cernst11 <cernst11@gmail.com>
2018-04-17 22:27:36 -0500

    Rewrite canvas exporter

--- js/CanvasClass.js
+++ js/CanvasClass.js
@@ -17,31 +17,84 @@ export default class CanvasClass {
     constructor(width, height) {
         this.height = height;
         this.width = width;
-        this.canvas = this.getCanvas();
+        this.canvas = document.querySelector('.mapboxgl-canvas');
+        this.oCanvas = document.querySelector('#overlay-canvas');
         this.compatibilityCheck();
 
         this.mapExport = document.querySelector('.map-export');
         this.overlayExport = document.querySelector('.overlay-export');
         this.compImageExport = document.querySelector('.comp-image');
         this.exportType = document.querySelector('.export-select');
+        this.cCanvas;
         autoBind(this);
         this.addEventListeners();
-
-   
     }
 
     addEventListeners() {
-        this.mapExport.addEventListener('click', this.exportMap);
-        this.overlayExport.addEventListener('click', this.exportOverlayCanvas);
-        this.compImageExport.addEventListener('click', this.generateImageComposite);
+        this.mapExport.addEventListener('click', this.exportMapToFile);
+        this.overlayExport.addEventListener('click', this.exportOverlayToFile);
+        this.compImageExport.addEventListener('click', this.exportCompositeToFile);
+
     }
 
-    /**
-     * Helper function to get the mapBox canvas. mapBox applies a style to the canvas
+    async canvasToPng(canvas) {
+        return await this.getCanvasBlob(canvas);
+    }
+
+    async canvasToTiff(canvas) {
+        return await this.getCanvasBlobTIFF(canvas);
+    }
+
+    async writeCanvasToFile(canvas, type = 'png') {
+        let blob = type === 'png' ? await this.canvasToPng(canvas) : await this.canvasToTiff(canvas);
+        FileSaver.saveAs(blob, `file.${type}`);
+    }
+
+    async exportMapToFile() {
+        this.writeCanvasToFile(this.canvas, this.getExportType().toLowerCase());
+    }
+
+    async exportOverlayToFile() {
+        this.writeCanvasToFile(this.oCanvas, this.getExportType().toLowerCase());
+    }
+
+    async exportCompositeToFile() {
+        this.createCompositeCanvas(this.getExportType().toLowerCase());
+        //this.writeCanvasToFile(this.canvas, this.getExportType().toLowerCase());
+    }
+
+        /**
+     * Composite the two files into a canvas and export the resulting composite 
+     * @param {string} mime export type
+     * @param {Number} [width] The width of the canvas
+     * @param {Number} [height] The height of the canvas
      */
-    getCanvas() {
-        let canvasList = document.getElementsByClassName("mapboxgl-canvas");
-        return canvasList[0];
+    async createCompositeCanvas(mime, width = 5625, height = 7500) {
+        this.drawOverlay()
+        let canvas = document.createElement('canvas');
+        canvas.id = "compositeCanvas";
+        canvas.width = width;
+        canvas.height = height;
+        canvas.style.zIndex = 8;
+        document.body.appendChild(canvas);
+
+        let mapImage = new window.Image();
+        var overlayImage = new window.Image();
+        let ctx = canvas.getContext('2d');
+
+        mapImage.addEventListener('load', () => {
+            ctx.drawImage(mapImage, 0, 0);
+            ctx.drawImage(overlayImage, 0, 0,
+                overlayImage.width,
+                overlayImage.height,
+                0, 0, canvas.width, canvas.height);
+            this.writeCanvasToFile(canvas, mime);
+            canvas.remove();
+            this.hideOverlay();
+        }, false);
+
+        mapImage.src = URL.createObjectURL(await this.canvasToPng(this.canvas));
+        overlayImage.src = URL.createObjectURL(await this.canvasToPng(this.oCanvas));
     }
 
     /**
@@ -53,146 +106,19 @@ export default class CanvasClass {
         });
     }
 
-    /**
-     * Generate a compostite image of the map and overlay
-     */
-    async generateImageComposite() {
-        let mapImg = URL.createObjectURL(await this.exportImageBlob(this.canvas));
-        let that = this;
-        const overlay = document.querySelector('#overlay-canvas');
-        overlay.toBlob(blob => {
-            let mapOverlay = URL.createObjectURL(blob);
-            let renderdComposite = that.createCompositeCanvas(mapImg, mapOverlay);
-        }, "image/png");           
-    }
-
-    /**
-     * Export a canvas as a png
-     * @param {HtmlElement} canvas The canvas element to export
-     * @param {string} imageType The type of image to export
-     */
-    async exportImageBlob(canvas, imageType = 'image/png') {
-        let dpi = 300;
-        return await this._getCanvasBlob(canvas, imageType);
-    }
-
-    /**
-     * Save blob export
-     */
-    async exportMap() {
-        const canvasToExport = this.canvas;
-        this.getExportType() === 'TIFF' ? this.saveToTiff(canvasToExport, true) : this.saveToPng(canvasToExport, true);
-    }
-
-    async saveToTiff(canvasToExport) {
-        CanvasToTIFF.toBlob(canvasToExport, blob => {
-            FileSaver.saveAs(blob, "map.tiff");
-        }, this.tiffExportProperties());
-    }
-
-    async saveToPng(canvasToExport) {
-        canvasToExport.toBlob(blob => {
-            FileSaver.saveAs(blob, "map.png");
-        }, "image/png");
-    }
-
-    /**
-     * Export the ovelay
-     */
-    exportOverlayCanvas() {
-        const overlay = document.querySelector('#overlay-canvas');
-        if (this.getExportType() === 'PNG') {
-            overlay.toBlob(blob => {
-                FileSaver.saveAs(blob, "map.png");
-            }, "image/png");
-        }else{
-            CanvasToTIFF.toBlob(overlay, blob => {
-                FileSaver.saveAs(blob, "overlay.tiff");
-            }, this.tiffExportProperties());
-        }
-    }
-
-    /**
-     * Create a temp div to hold the mapOverlay during export
-     */
-    _createTempDiv() {
-        let contDiv = document.getElementsByClassName('map-overlay')[0];
-        let cloneDiv = contDiv.cloneNode(true);
-        document.body.append(cloneDiv);
-
-        let newDiv = document.getElementsByClassName('map-overlay')[1];
-        newDiv.style = 'transform: scale(1.0)!important';
-        newDiv.style.height = '2400px';
-        newDiv.style.width = '1800px';
-
-        return newDiv;
-
-    }
-
-    /**
-     * Turn the callback into a promoise
-     * @param {Object} canvas The canvas to turn into blob
-     */
-    async _getCanvasBlob(canvas) {
-        return new Promise(function (resolve, reject) {
-            canvas.toBlob(function (blob) {
+    async getCanvasBlob(canvas, type = 'image/png') {
+        return new Promise((resolve, reject) => {
+            canvas.toBlob((blob) => {
                 resolve(blob)
-            })
+            }, type)
         })
     }
-
-
-
-    /**
-     * Composite the two files into a canvas and export the resulting composite 
-     * @param {Object} map The map to export
-     * @param {Object} overlay The overlay to export
-     * @param {Number} [width] The width of the canvas
-     * @param {Number} [height] The height of the canvas
-     */
-    async createCompositeCanvas(map, overlay, width = 5625, height = 7500, ) {
-        try {
-            let canvas = document.createElement('canvas');
-            canvas.id = "compositeCanvas";
-            canvas.width = width;
-            canvas.height = height;
-            canvas.style.zIndex = 8;
-            document.body.appendChild(canvas);
-
-            let mapImg = new window.Image();
-            var overlayImg = new window.Image();
-
-            mapImg.addEventListener('load', () => {
-                canvas.getContext('2d').drawImage(mapImg, 0, 0);
-                //scale the overlay to match the map 
-                canvas.getContext('2d').drawImage(overlayImg, 0, 0,
-                    overlayImg.width,
-                    overlayImg.height,
-                    0, 0, canvas.width, canvas.height);
-                this.getExportType() === 'PNG' ? this.compositePNGExport(canvas) : this.compositeTiffExport(canvas);
-            });
-
-            mapImg.setAttribute("src", map);
-            overlayImg.setAttribute("src", overlay);
-
-            return {
-                completed: true,
-                errors: null
-            };
-
-        } catch (e) {
-            return {
-                completed: false,
-                errors: e
-            };
-        }
-    }
-
-    compositeTiffExport(canvas) {
-        CanvasToTIFF.toBlob(canvas, blob => {
-            FileSaver.saveAs(blob, "composite.tiff");
-            canvas.parentNode.removeChild(canvas);
-        }, this.tiffExportProperties());
+    async getCanvasBlobTIFF(canvas) {
+        return new Promise((resolve, reject) => {
+            CanvasToTIFF.toBlob(canvas, blob => {
+                resolve(blob)
+            }, this.tiffExportProperties());
+        })
     }
 
     compatibilityCheck() {
@@ -204,7 +130,7 @@ export default class CanvasClass {
     drawOverlay() {
         let overlay = document.createElement('div');
         overlay.id = "overlay";
-        document.getElementsByTagName('body')[0].appendChild(overlay);
+        document.querySelector('body').appendChild(overlay);
     }
 
     hideOverlay() {
@@ -239,12 +165,5 @@ export default class CanvasClass {
                 return dpi / 96
             }
         });
-    }
-
-    compositePNGExport(canvas, fileType) {
-        canvas.toBlob(blob => {
-            FileSaver.saveAs(blob, 'composite.png');
-            canvas.parentNode.removeChild(canvas);
-        }, 'image/png');
     }
 }
